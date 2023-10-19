1b6944ac473a663efb22060875dcd220a08e298d
davidagina <davidagina64@gmail.com>
2023-10-19 12:27:08 +0100

    Added StudentDto class

---
+++ src/main/java/com/daveproject/sms/dto/StudentDto.java
@@ -0,0 +1,24 @@
+package com.daveproject.sms.dto;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class StudentDto {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String department;
+}
