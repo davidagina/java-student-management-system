46b218f43446da4a2bae78928694d8179960b363
davidagina <davidagina64@gmail.com>
2023-10-19 13:04:35 +0100

    Added StudentController class

---
+++ src/main/java/com/daveproject/sms/controller/Controller.java
@@ -0,0 +1,12 @@
+package com.daveproject.sms.controller;
+
+import com.daveproject.sms.dto.StudentDto;
+import com.daveproject.sms.entity.Student;
+import com.daveproject.sms.service.StudentService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+}
