97572a062973fe2c8edb01e48336b09b2086d69e
davidagina <davidagina64@gmail.com>
2023-10-19 13:05:00 +0100

    Added StudentServiceImpl class

---
+++ src/main/java/com/daveproject/sms/service/impl/StudentServiceImpl.java
@@ -0,0 +1,9 @@
+package com.daveproject.sms.service.impl;
+
+import com.daveproject.sms.entity.Student;
+import com.daveproject.sms.service.StudentService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentServiceImpl implements StudentService {
+}
