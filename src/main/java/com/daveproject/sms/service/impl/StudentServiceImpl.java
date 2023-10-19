6e8b43c7597e0651b74c1dfb07f0f661c614bc99
davidagina <davidagina64@gmail.com>
2023-10-19 21:42:15 +0100

    createStudent method added

--- src/main/java/com/daveproject/sms/service/impl/StudentServiceImpl.java
+++ src/main/java/com/daveproject/sms/service/impl/StudentServiceImpl.java
@@ -29,4 +29,10 @@ public class StudentServiceImpl implements StudentService {
 
         return studentDtos;
     }
+
+    @Override
+    public void createStudent(StudentDto studentDto) {
+        Student student = StudentMapper.mapToStudent(studentDto);
+        studentRepository.save(student);
+    }
 }
