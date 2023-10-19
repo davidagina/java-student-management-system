d19c8426243001c6e6561f2c88c447d7f3138a67
davidagina <davidagina64@gmail.com>
2023-10-19 15:46:42 +0100

    Updated

--- src/main/java/com/daveproject/sms/controller/StudentController.java
+++ src/main/java/com/daveproject/sms/controller/StudentController.java
@@ -1,22 +1,19 @@
 package com.daveproject.sms.controller;
 
 import com.daveproject.sms.dto.StudentDto;
-import com.daveproject.sms.entity.Student;
 import com.daveproject.sms.service.StudentService;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
-public class Controller {
+@Controller
+public class StudentController {
 
     private StudentService studentService;
 
-    public Controller(StudentService studentService) {
+    public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
 
