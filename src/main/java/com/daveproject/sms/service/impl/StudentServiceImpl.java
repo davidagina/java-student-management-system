df536e3dae0a781b8d1612ce849a80fab7b7b032
davidagina <davidagina64@gmail.com>
2023-10-19 15:11:38 +0100

    Added getAllStudents method

--- src/main/java/com/daveproject/sms/service/impl/StudentServiceImpl.java
+++ src/main/java/com/daveproject/sms/service/impl/StudentServiceImpl.java
@@ -1,9 +1,32 @@
 package com.daveproject.sms.service.impl;
 
+import com.daveproject.sms.dto.StudentDto;
 import com.daveproject.sms.entity.Student;
+import com.daveproject.sms.mappper.StudentMapper;
+import com.daveproject.sms.repository.StudentRepository;
 import com.daveproject.sms.service.StudentService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
 public class StudentServiceImpl implements StudentService {
+
+    private StudentRepository studentRepository;
+
+    public StudentServiceImpl(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    @Override
+    public List<StudentDto> getAllStudents() {
+        List<Student> students = studentRepository.findAll();
+
+        List<StudentDto> studentDtos = students.stream()
+                .map((student) -> StudentMapper.mapToStudentDto(student))
+                .collect(Collectors.toList());
+
+        return studentDtos;
+    }
 }
