c2117fe40b21084e725931c8671ca9c20b5397df
davidagina <davidagina64@gmail.com>
2023-10-20 03:44:34 +0100

    Added validation

--- src/main/java/com/daveproject/sms/controller/StudentController.java
+++ src/main/java/com/daveproject/sms/controller/StudentController.java
@@ -2,9 +2,12 @@ package com.daveproject.sms.controller;
 
 import com.daveproject.sms.dto.StudentDto;
 import com.daveproject.sms.service.StudentService;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -37,7 +40,13 @@ public class StudentController {
     }
 
     @PostMapping("/students")
-    public String saveStudent(StudentDto student){
+    public String saveStudent(@Valid @ModelAttribute("student") StudentDto student,
+                              BindingResult result, Model model){
+
+        if(result.hasErrors()){
+            model.addAttribute("student", student);
+            return "create_student";
+        }
         studentService.createStudent(student);
         return "redirect:/students";
     }
