6ba5c9bc6e692427c5c67103ecbed7d325a12b9f
davidagina <davidagina64@gmail.com>
2023-10-20 03:44:42 +0100

    Added validation

--- src/main/java/com/daveproject/sms/dto/StudentDto.java
+++ src/main/java/com/daveproject/sms/dto/StudentDto.java
@@ -3,6 +3,8 @@ package com.daveproject.sms.dto;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -17,8 +19,13 @@ public class StudentDto {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @NotEmpty(message = "Please fill student\'s first name")
     private String firstName;
+    @NotEmpty(message = "Please fill student\'s last name")
     private String lastName;
+    @NotEmpty(message = "Please fill student\'s email")
+    @Email
     private String email;
+    @NotEmpty(message = "Please fill student\'s department")
     private String department;
 }
