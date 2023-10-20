3c6a7935710437b2c296e679d16208417d1c947e
davidagina <davidagina64@gmail.com>
2023-10-20 06:14:09 +0100

    getStudentById & updateStudent added

--- src/main/java/com/daveproject/sms/service/impl/StudentServiceImpl.java
+++ src/main/java/com/daveproject/sms/service/impl/StudentServiceImpl.java
@@ -8,6 +8,7 @@ import com.daveproject.sms.service.StudentService;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
@@ -34,5 +35,17 @@ public class StudentServiceImpl implements StudentService {
     public void createStudent(StudentDto studentDto) {
         Student student = StudentMapper.mapToStudent(studentDto);
         studentRepository.save(student);
+    }
+
+    @Override
+    public StudentDto getStudentById(Long studentId) {
+        Student student = studentRepository.findById(studentId).get();
+        StudentDto studentDto = StudentMapper.mapToStudentDto(student);
+        return studentDto;
+    }
+
+    @Override
+    public void updateStudent(StudentDto studentDto) {
+        studentRepository.save(StudentMapper.mapToStudent(studentDto));
     }
 }
